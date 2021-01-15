d664b05eb710034bec920b979619ce7bf51a5467
Evgeniu603 <72415121+Evgeniu603@users.noreply.github.com>
2021-01-15 12:36:51 +0200

    Update Manager.java

--- src/domain/Manager.java
+++ src/domain/Manager.java
@@ -1,6 +1,17 @@
 package domain;
+/**
+ * public class "manager" for desc of manager
+ * @author User
+ */
 public class Manager extends Employee {
-
+    /**
+     * Constructor 1
+     * @param employees
+     * @param name
+     * @param jobTitle
+     * @param level
+     * @param dept 
+     */
     public Manager(Employee[] employees, String name, String jobTitle, int level, String dept) {
         super(name, jobTitle, level, dept);
         this.employees = employees;
@@ -11,18 +22,28 @@ public class Manager extends Employee {
         return super.toString()+"\nEmployees: "+getEmployees(); 
     }
 
+    /**
+     * Constructor 2
+     * @param employees 
+     */
     public Manager(Employee[] employees) {
         super();
         this.employees = employees;
     }
     
+    /**
+     * Constructor 3
+     */
     public Manager() {
         super();
         employees = new Employee[10];
     }
-
     private Employee[] employees;
 
+    /**
+     * Method for getting list of employees
+     * @return s
+     */
     public String getEmployees() {
         String s = "";
         for (Employee e : employees) {
@@ -32,13 +53,19 @@ public class Manager extends Employee {
         return s;
     }
 
+    /**
+     * Method for setting list of employees
+     * @param employees one position in employee's list
+     */
     public void setEmployees(Employee[] employees) {
         this.employees=employees;
     }
 
+    /**
+     * Method for getting employees list
+     * @return employees
+     */
     public Employee[] getEmployeesList() {
         return employees;
     }
-
-    
 }
